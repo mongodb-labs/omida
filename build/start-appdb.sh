f9d279be19bfc9bc30f7208439410f43e1d5bd3c
Mihai Bojin <mihai.bojin@mongodb.com>
2022-06-22 23:01:23 +0100

    Improve arg parsing

--- build/start-appdb.sh
+++ build/start-appdb.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 
-if [[ "$#" -lt 1 ]]; then
-    echo "You must specify a MongoDB version as the first parameter!"
-    echo "Usage: ./start-appdb.sh 5"
+mdb_version=
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --mdb-version) mdb_version="${2-}"; shift 2 ;;
+        * ) echo "Invalid configuration option: '$1'"; return 1 ;;
+    esac
+done
+
+if [[ -z "$mdb_version" ]]; then
+    echo "You must specify a MongoDB version to use!"
+    echo "Usage: start-appdb.sh --mdb_version 5"
     echo
     exit 1
 fi
 
 echo
-echo "Starting a container for the AppDB (mongo:$1)..."
+echo "Starting a container for the AppDB (mongo:$mdb_version)..."
 if ! docker start appdb; then
     echo "Did not find a previously running container, or could not restart it."
     echo "Starting AppDB from scratch..."
-    docker run --net=bridge --name appdb -p 27017:37017/tcp -d mongo:"$1"
+    docker run --net=bridge --name appdb -p 27017:37017/tcp -d mongo:"$mdb_version"
 fi
 
 APPDB_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' appdb)
